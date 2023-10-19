935ebf05a27e219d3301cea9ba35709b8154cd4e
renaldox <akeripa.pose@gmail.com>
2023-10-19 18:34:48 +1300

    make it a card

--- src/lib/Components/Contributors.svelte
+++ src/lib/Components/Contributors.svelte
@@ -2,15 +2,16 @@
     import { Avatar } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="container flex flex-col items-center pb-5">
-    <Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-32" rounded="rounded-3xl" />
-    <h5 class="mb-2 text-lg font-bold">Maria Smantha</h5>
-    <h6 class="mb-4 font-medium text-primary dark:text-primary-400">Web Developer</h6>
+<div class="container flex flex-col items-center pb-5 card card-hover py-10 mx-0">
+    <Avatar src="https://images.unsplash.com/photo-1617296538902-887900d9b592?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzExMDB8&ixlib=rb-4.0.3&w=128&h=128&auto=format&fit=crop" width="w-32" rounded="rounded-3xl"/>
+    <h5 class="mb-2 text-lg font-bold card-header">Maria Smantha</h5>
+    <h6 class="mb-4 font-medium text-primary dark:text-primary-400 p-4">Web Developer</h6>
     <svg class="w-10 h-10 mx-auto mb-3 text-gray-400 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
         <path d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
     </svg>
     <blockquote>
-        <p class="text-2xl italic font-medium text-gray-900 dark:text-white">"This is my favorite quote"</p>
+        <p class="text-2xl italic font-medium text-gray-900 dark:text-white p-4">"This is my favorite quote"</p>
     </blockquote>
 
 </div>
+  
