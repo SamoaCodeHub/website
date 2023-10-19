48390dcce40fcc6059ad7a261a2f9db1be35edb7
Andre Westerlund <andrewest_04@hotmail.com>
2023-10-20 00:32:29 +1300

    rebase off main and add media to home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Hero from "$lib/components/Hero.svelte";
+	import Media from "$lib/components/Media.svelte";
 	import Contact from "$lib/components/Contact.svelte";
 	import About from '$lib/components/About.svelte';
 </script>
 
 <Hero/>
 <About />
+<Media/>
 <Contact/>
 
 <style lang="postcss">
