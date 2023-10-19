00ad099320c01b4a5f45f18cc01efbd8d96d7c8b
Chia Lin <chia.lin685@gmail.com>
2023-10-19 19:43:19 +1300

    WEB-20 add hero section

---
+++ src/lib/components/Hero.svelte
@@ -0,0 +1,48 @@
+<section class="py-24 min-h-[90%] text-white">
+	<div class="max-w-screen-xl mx-auto px-10">
+		<div class="flex flex-col md:flex-row items-center space-x-0 ">
+			<div class="flex flex-col w-full md:w-2/3">
+				<div class="flex flex-col space-y-4">
+					<h1 class="text-7xl font-bold font-heading text-center md:text-left">
+						Innovate,
+						<div class="relative py-4">
+							<span class="relative z-10 inline-block">
+								Collaborate
+								<span
+									class="absolute top-2 left-0 md:left-3 md:top-3 w-full h-full bg-[#dd5757] rounded-xl z-[-999]"
+								/>
+							</span>
+						</div>
+						Create
+					</h1>
+					<p class="text-xl text-center md:text-left">
+						Samoa Stack Overflow - Where Innovation meets Collaboration
+					</p>
+				</div>
+				<div class="flex flex-col gap-4 lg:flex-row `mt-12 space-x-0 md:space-x-3 items-center">
+					<button class="py-4 w-full sm:w-1/3 lg:w-1/3 bg-blue-500 rounded-xl uppercase font-bold"
+						>Learn More</button
+					>
+					<div class="flex items-center justify-center gap-2 w-full lg:w-2/3">
+						<img src="/images/samoan-flag.svg" class="w-14 h-12" alt="Samoan flag" />
+						<div class="flex flex-col">
+							<p class="text-sm text-gray-300 font-extrabold">A community of software developers</p>
+							<p class="text-base text-gray-400">+20 contributing members</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="w-full md:w-2/3 pt-10">
+				<img src="/images/sso-banner-img.svg" class="w-full h-full" alt="Hero Section" />
+			</div>
+		</div>
+	</div>
+</section>
+
+<style lang="postcss">
+	section {
+		background-image: url('/images/hero-bg.svg');
+		background-size: cover;
+		background-position: center;
+	}
+</style>
