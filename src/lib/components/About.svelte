36896acbde87b74e79132fce256de56ab5ce589c
Ken2523 <kendricklui032517@gmail.com>
2023-11-02 19:17:46 +1300

    about-section page

---
+++ src/lib/components/About.svelte
@@ -0,0 +1,97 @@
+<section class="py-24 min-h-[90%] text-white">
+	<div class="max-w-screen-xl px-10 mx-auto">
+		<div class="flex flex-col items-center space-x-0 md:flex-row ">
+			<div class="flex flex-col w-full md:w-2/3">
+				<div class="flex flex-col space-y-4">
+					<h1 class="font-bold text-center text-7xl font-heading md:text-left">
+						About Us
+						<!-- <div class="relative py-4">
+							<span class="relative z-10 inline-block">
+								Collaborate
+								<span
+									class="absolute top-2 left-0 md:left-3 md:top-3 w-full h-full bg-[#dd5757] rounded-xl z-[-999]"
+								/>
+							</span>
+						</div>
+						Create -->
+					</h1>
+					<p class="text-xl text-center md:text-left">
+						Introducing Samoa Stack Overflow! 
+					</p>
+				</div>
+				<div class="flex flex-col gap-4 lg:flex-row `mt-12 space-x-0 md:space-x-3 items-center">
+					<button class="w-full py-4 font-bold uppercase bg-blue-500 sm:w-1/3 lg:w-1/3 rounded-xl"
+						>Learn More</button
+					>
+					<div class="flex items-center justify-center w-full gap-2 lg:w-2/3">
+						<img src="/images/samoan-flag.svg" class="h-12 w-14" alt="Samoan flag" />
+						<div class="flex flex-col">
+							<p class="text-sm font-extrabold text-gray-300">A community of software developers</p>
+							<p class="text-base text-gray-400">+20 contributing members</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="w-full pt-10 md:w-2/3">
+				<img src="/images/sso-banner-img.svg" class="w-full h-full" alt="Hero Section" />
+			</div>
+		</div>
+        <div class="grid gap-8">
+			<div>
+				<p class="text-xl text-center md:text-left">
+					Hey everyone! We're excited to welcome you to Samoa Stack Overflow (SSO), your go-to tech community in our beautiful island nation!
+				</p>
+			</div>
+			<div>
+				<p class="text-xl text-center md:text-left">
+					Our mission is simple: to bring together passionate developers like you, foster collaboration, and provide amazing opportunities for growth in the world of technology. Whether you're a seasoned pro or just starting out, SSO is the place for you!
+				</p>
+			</div>
+			<div>
+				<p class="text-xl text-center md:text-left">
+					Join us on this incredible journey as we learn, build, and share technology together! Here's what you can expect from SSO:
+				</p>
+				<ol class="max-w-md space-y-1 ml-4 text-gray-500 list-decimal list-inside dark:text-gray-400">
+					<li class="py-4">
+						<span class="font-semibold text-gray-900 dark:text-white">Engaging Workshops: Our talented mentors will lead workshops focused on specific skills, helping you level up and stay ahead of the game.</span>
+					</li>
+					<li class="py-4">
+						<span class="font-semibold text-gray-900 dark:text-white">Coding Classes for Kids: Introduce programming at the primary level, nurturing the future generation of tech wizards.</span>
+					</li>
+					<li class="py-4">
+						<span class="font-semibold text-gray-900 dark:text-white">Thrilling Hackathons: Together, we'll tackle real-world problems faced by our local community, businesses, and the public sector. Let's innovate and make a positive impact! </span>
+					</li>
+					<li class="py-4">
+						<span class="font-semibold text-gray-900 dark:text-white">Job Opportunities: We're creating a digital platform exclusively for developer-related jobs, internships, and other exciting career prospects. Open doors to tech companies in Samoa! </span>
+					</li>
+					<li class="py-4">
+						<span class="font-semibold text-gray-900 dark:text-white">Collaborative Website Project: Join us in building an amazing SSO website using collaboration tools like GitHub, Trello, and Jira. Together, we'll show the world what we're made of! </span>
+					</li>
+				</ol>
+			</div>
+			<div>
+				<p class="text-xl text-center md:text-left">
+					So, what are you waiting for? Jump into our thriving Discord community, connect with fellow developers, and let's shape the future of technology in Samoa! 
+				</p>
+			</div>
+			<div>
+				<p class="text-xl text-center md:text-left">
+					Stay tuned for more updates and exciting events. Together, we'll unlock endless possibilities in the world of tech! 
+				</p>
+			</div>
+			<div>
+				<p class="text-xl italic text-center md:text-left">
+					#SSO #TechCommunity #JoinTheJourney
+				</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<style lang="postcss">
+	section {
+		background-image: url('/images/hero-bg.svg');
+		background-size: cover;
+		background-position: center;
+	}
+</style>
