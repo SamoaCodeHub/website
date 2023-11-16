01ad3ba1a6ce214634342f07199a7405dd17368f
Chia Lin <chia.lin685@gmail.com>
2023-11-16 18:33:54 +1300

    make hero section responsive

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,11 +1,11 @@
-<section class="py-24 min-h-[90%] text-white">
-	<div class="max-w-screen-xl mx-auto px-10">
-		<div class="flex flex-col md:flex-row items-center space-x-0 ">
-			<div class="flex flex-col w-full md:w-2/3">
+<section class="py-8 md:py-24 text-white">
+	<div class="max-w-screen-xl mx-auto px-3 md:px-10">
+		<div class="flex flex-col xl:flex-row items-center space-x-0">
+			<div class="flex flex-col w-full xl:w-2/3">
 				<div class="flex flex-col space-y-4">
-					<h1 class="text-7xl font-bold font-heading text-center md:text-left">
+					<h1 class="text-4xl sm:text-6xl md:text-7xl font-bold font-heading text-center xl:text-left">
 						Innovate,
-						<div class="relative py-4">
+						<div class="relative py-2 xl:py-4">
 							<span class="relative z-10 inline-block">
 								Collaborate
 								<span
@@ -15,24 +15,35 @@
 						</div>
 						Create
 					</h1>
-					<p class="text-xl text-center md:text-left">
+					<p class="text-md xl:text-xl text-center xl:text-left">
 						Samoa Stack Overflow - Where Innovation meets Collaboration
 					</p>
 				</div>
-				<div class="flex flex-col gap-4 lg:flex-row mt-12 space-x-0 md:space-x-3 items-center">
-					<button class="py-4 w-full sm:w-1/3 lg:w-1/3 bg-blue-500 rounded-xl uppercase font-bold"
-						>Learn More</button
-					>
-					<div class="flex items-center justify-center gap-2 w-full lg:w-2/3">
-						<img src="/images/samoan-flag.svg" class="w-14 h-12" alt="Samoan flag" />
-						<div class="flex flex-col">
-							<p class="text-sm text-gray-300 font-extrabold">A community of software developers</p>
-							<p class="text-base text-gray-400">+20 contributing members</p>
+
+				<div
+					class="mt-4 w-[80%] lg:w-[100%] mx-auto xl:mx-0 lg:max-w-[600px] flex sm:flex-row flex-col items-center sm:gap-8 gap-4"
+				>
+					<div class="flex-1 sm:w-auto w-full">
+						<p class="sr-only">
+							Press Join us Now to find out more about becoming a Samoa Stack Overflow Member
+						</p>
+						<button class="btn bg-blue-500 w-full rounded-xl">Join us now</button>
+					</div>
+					<div class="flex-1 flex flex-row sm:gap-2 text-sm sm:text-base justify-center">
+						<div
+							class="flex-1 flex sm:flex-col flex-row sm:gap-0 gap-1 text-sm sm:text-base justify-center"
+						>
+							<p class="font-semibold">
+								A community of software developers<span class="sm:hidden inline">.</span>
+							</p>
+							<p class="text-base-content/70">
+								+20 contributing members<span class="sm:hidden inline">.</span>
+							</p>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div class="w-full md:w-2/3 pt-10">
+			<div class="w-full xl:w-2/3 pt-2">
 				<img src="/images/sso-banner-img.svg" class="w-full h-full" alt="Hero Section" />
 			</div>
 		</div>
