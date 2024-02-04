63ec283bdf0fd2d1f8370490d13e190c85c92267
Andre Westerlund <andrewest_04@hotmail.com>
2024-02-05 12:30:11 +1300

    remove IconBrandDiscordFilled not used to resolve lint error

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Hero from '$lib/components/Hero.svelte';
-	import Media from "$lib/components/Media.svelte";
+	import Media from '$lib/components/Media.svelte';
 	import Contact from '$lib/components/Contact.svelte';
 </script>
 
