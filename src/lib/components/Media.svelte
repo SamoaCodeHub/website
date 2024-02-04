8e54dea65a5b6a41e9f8979ecda170f356090e45
Andre Westerlund <andrewest_04@hotmail.com>
2024-02-05 12:30:11 +1300

    remove IconBrandDiscordFilled not used to resolve lint error

--- src/lib/components/Media.svelte
+++ src/lib/components/Media.svelte
@@ -1,8 +1,5 @@
 <script>
-	import encode from '../assets/images/encode.jpg';
-	import eugeneWorkshop from '../assets/images/eugene_workshop.jpg';
 	import lunchSita from '../assets/images/lunch_sita.jpg';
-	import sitaExpo from '../assets/images/sita_expo.jpg';
 	import sprintMeeting from '../assets/images/sprint_meeting.jpg';
 	import ssoLunch1 from '../assets/images/sso_lunch.jpg';
 	import ssoLunch2 from '../assets/images/sso_lunch_2.jpg';
@@ -10,45 +7,58 @@
 	import workshop from '../assets/images/workshop.jpg';
 </script>
 
-
-<section class="py-24 text-white">
-	<div class="max-w-screen-xl mx-auto px-10">
-		<div class="container mx-auto py-16">
-      <h1 class="text-center text-5xl text-gray-900 font-bold border-yellow-50 dark:text-gray-600 pb-[10%]">Samoa Stack Overflow Media</h1>
-			<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-				<div class="p-2 rounded-lg -translate-y-16">
-					<img src={ssoLunch1} alt="SSO Lunch at Amanakis" class="w-full h-auto img-card" />
-					<img src={ssoLunch2} alt="SSO Lunch at Coffee Roaster" class="w-full h-auto img-card" />
-					<img src={encode} alt="Ainsof and Encode team at SITA Expo" class="w-full h-auto img-card" />
+<section class="w-full px-5 py-2 lg:px-32 lg:py-24 text-white">
+	<div class="container mx-auto">
+		<h1 class="text-center h1 font-bold border-yellow-50 py-7 md:pb-[5%]">
+			Samoa Stack Overflow <br /> Meet Ups
+		</h1>
+		<div class="-m-1 flex flex-wrap md:-m-2">
+			<div class="flex w-full flex-wrap lg:w-1/2">
+				<div class="w-full p-1 md:p-2 md:w-1/2">
+					<img
+						alt="gallery"
+						class="block h-full w-full rounded-lg object-cover object-center"
+						src={ssoLunch3}
+					/>
 				</div>
-
-				<div class="p-2 rounded-lg">
-					<img src={sprintMeeting} alt="Sprint Meeting at Vodafone Samoa" class="w-full h-auto img-card" />
-					<img src={ssoLunch3} alt="SSO Lunch at Phat Burger" class="w-full h-auto img-card" />
-					<img src={eugeneWorkshop} alt="Workshop led by Eugene Barkman" class="w-full h-auto img-card" />
+				<div class="w-full p-1 md:p-2 md:w-1/2">
+					<img
+						alt="gallery"
+						class="block h-full w-full rounded-lg object-cover object-center"
+						src={lunchSita}
+					/>
 				</div>
-
-				<div class="p-2 rounded-lg -translate-y-16">
-					<img src={sitaExpo} alt="Sita Expo Team" class="w-full h-auto img-card" />
-					<img src={workshop} alt="SITA Workshop" class="w-full h-auto img-card" />
-					<img src={lunchSita} alt="SITA and SSO Lunch at Rose Cafe" class="w-full h-auto img-card" />
+				<div class="w-full p-1 md:p-2">
+					<img
+						alt="gallery"
+						class="block h-full w-full rounded-lg object-cover object-center"
+						src={workshop}
+					/>
+				</div>
+			</div>
+			<div class="flex w-full flex-wrap lg:w-1/2">
+				<div class="w-full p-1 md:p-2">
+					<img
+						alt="gallery"
+						class="block h-full w-full rounded-lg object-cover object-center"
+						src={ssoLunch1}
+					/>
+				</div>
+				<div class="w-full p-1 md:p-2 md:w-1/2">
+					<img
+						alt="gallery"
+						class="block h-full w-full rounded-lg object-cover object-center"
+						src={ssoLunch2}
+					/>
+				</div>
+				<div class="w-full p-1 md:p-2 md:w-1/2">
+					<img
+						alt="gallery"
+						class="block h-full w-full rounded-lg object-cover object-center"
+						src={sprintMeeting}
+					/>
 				</div>
 			</div>
 		</div>
 	</div>
 </section>
-
-
-<style>
-  .img-card {
-    border-radius: 10px;
-    margin: 1.3rem 0;
-  }
-<style lang="postcss">
-  section {
-		background-image: url('/images/tech-bg.png');
-		background-size: cover;
-		background-position: top;
-    background-repeat: no-repeat;
-	}
-</style>
