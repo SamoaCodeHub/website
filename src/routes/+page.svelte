b99355ee29a9824b7991a077be8307d91e47ca7c
renaldox <akeripa.pose@gmail.com>
2024-02-07 16:14:23 +1300

    utilised a list for dev profiles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,10 @@
 	import Hero from '$lib/components/Hero.svelte';
 	import Media from '$lib/components/Media.svelte';
 	import Contact from '$lib/components/Contact.svelte';
+	import Contributors from '$lib/components/Contributors.svelte';
 </script>
 
 <Hero />
 <Media />
 <Contact />
+<Contributors />
