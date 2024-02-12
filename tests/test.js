547ab371eb13605e8dfc4b380153983d8bf17af8
Alesana Eteuati Jr <junior.eteuati@gmail.com>
2024-02-13 11:24:11 +1300

    [tests] some simple e2e tests

--- tests/test.js
+++ tests/test.js
@@ -1,6 +1,22 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+test('Hero section has expected p', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByText('Press Join us Now to find out more about becoming a Samoa Stack Overflow Member')).toBeVisible();
 });
+
+test('Media section has expected h1', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByText('Samoa Stack Overflow Meet Ups')).toBeVisible();
+});
+
+test('Contact section has expected h1', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByText('Contact Us')).toBeVisible();
+});
+
+test('Contributors section has expected h1', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByText('Meet our Seki Devs')).toBeVisible();
+});
+
