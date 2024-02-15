73be6023e5a93d68fc8214c117c21c57af679b41
shifu-lin <chia.lin685@gmail.com>
2024-02-15 19:22:37 +1300

    Changed vertical-timeline package
    install svelte-vertical-timeline

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,66 +1,74 @@
 <script>
 	import { IconDiscountCheckFilled } from '@tabler/icons-svelte';
+	import {
+		Timeline,
+		TimelineItem,
+		TimelineSeparator,
+		TimelineDot,
+		TimelineConnector,
+		TimelineContent,
+		TimelineOppositeContent
+	} from 'svelte-vertical-timeline';
+	const options = [
+		{
+			title: 'Launched SSO Facebook Group',
+			time: 'August 2021',
+			description:
+				'Created SSO Facebook group as a central hub for information sharing within our community'
+		},
+		{
+			title: 'Created SSO Discord Channel',
+			time: 'September 2021',
+			description:
+				'Started a Discord Channel facilitating streamlined communication and collaboration for SSO members'
+		},
+		{
+			title: 'Completed SITA Membership Database Project',
+			time: 'August 2023',
+			description: 'Successfully wrapped up the SITA membership database project'
+		},
+		{
+			title: 'Launched SSO Website',
+			time: 'March 2024',
+			description:
+				'Unveiled the official SSO Website, streamlining user access and enhacing information sharing within'
+		}
+	];
 </script>
 
 <!-- component -->
-<div class="container bg-transparent py-10 mx-auto w-full">
-	<h1 class="text-4xl font-bold text-center">A brief history of Samoa Stack Overflow</h1>
-	<div class="relative wrap overflow-hidden p-10 h-full">
-		<div
-			class="border-2-2 absolute border-opacity-20 border-gray-200 h-full border"
-			style="left: 50%"
-		/>
-		<!-- right timeline -->
-		<div class="mb-8 flex justify-between items-center w-full right-timeline">
-			<div class="order-1 w-5/12" />
-			<div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-				<h1 class="mx-auto font-semibold text-lg text-white"><IconDiscountCheckFilled /></h1>
-			</div>
-			<div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-				<h3 class="font-bold text-gray-800 text-xl">Launched SSO Facebook Group</h3>
-				<h4 class="mb-2 text-sm font-semibold text-gray-700">16 August 2021</h4>
-			</div>
-		</div>
-
-		<!-- left timeline -->
-		<div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-			<div class="order-1 w-5/12" />
-			<div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-				<h1 class="mx-auto text-white font-semibold text-lg"><IconDiscountCheckFilled /></h1>
-			</div>
-			<div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-				<h3 class="font-bold text-white text-xl">Created SSO Discord Channel</h3>
-				<h4 class="mb-2 text-sm font-semibold text-gray-700">16 August 2021</h4>
-			</div>
-		</div>
-
-		<!-- right timeline -->
-		<div class="mb-8 flex justify-between items-center w-full right-timeline">
-			<div class="order-1 w-5/12" />
-			<div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-				<h1 class="mx-auto font-semibold text-lg text-white"><IconDiscountCheckFilled /></h1>
-			</div>
-			<div class="order-1 bg-gray-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-				<h3 class="font-bold text-gray-800 text-xl">Completed SITA Membership Database Project</h3>
-				<h4 class="mb-2 text-sm font-semibold text-gray-700">16 August 2021</h4>
-			</div>
-		</div>
-
-		<!-- left timeline -->
-		<div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
-			<div class="order-1 w-5/12" />
-			<div class="z-20 flex items-center order-1 bg-gray-800 shadow-xl w-8 h-8 rounded-full">
-				<h1 class="mx-auto text-white font-semibold text-lg"><IconDiscountCheckFilled /></h1>
-			</div>
-			<div class="order-1 bg-red-400 rounded-lg shadow-xl w-5/12 px-6 py-4">
-				<h3 class="font-bold text-white text-xl">Launched Official SSO Website</h3>
-				<h4 class="mb-2 text-sm font-semibold text-gray-700">05 December 2025</h4>
-				<!-- <p class="text-sm font-medium leading-snug tracking-wide text-white text-opacity-100">
-					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-					been the industry's standard dummy text ever since the 1500s, when an unknown printer took
-					a galley of type and scrambled it to make a type specimen book.
-				</p> -->
-			</div>
-		</div>
-	</div>
+<div class="container bg-transparent py-12 mx-auto w-full">
+	<h1 class="text-4xl font-bold text-center">Samoa Stack Overflow - Our Journey in Code</h1>
+	<Timeline position="alternate" style={'padding: 50px 0;'}>
+		{#each options as option}
+			<TimelineItem>
+				<TimelineOppositeContent slot="opposite-content">
+					<p>{option.time}</p>
+				</TimelineOppositeContent>
+				<TimelineSeparator>
+					<TimelineDot style={'background-color: #7CD5E2;'} />
+					<TimelineConnector />
+				</TimelineSeparator>
+				<TimelineContent style={'padding-bottom: 50px;'}>
+					<h3>{option.title}</h3>
+					<p>{option.description}</p>
+				</TimelineContent>
+			</TimelineItem>
+		{/each}
+	</Timeline>
 </div>
+
+<style>
+	h3 {
+		letter-spacing: 1.5px;
+		margin: 0;
+		padding-bottom: 10px;
+		font-size: 18px;
+	}
+	p {
+		letter-spacing: 1.5px;
+		margin: 0;
+		color: #d3d3d3;
+		font-size: 14px;
+	}
+</style>
