506486a5ee5f9d8f7c4e2bf856f428a9d6d07818
tausani93 <tausani93@gmail.com>
2024-02-18 01:04:28 +1300

    Capitalise > uppercase

--- src/lib/components/Contributors.svelte
+++ src/lib/components/Contributors.svelte
@@ -77,7 +77,7 @@
 	];
 </script>
 
-<section id={SectionName.CONTRIBUTORS} class="container mx-auto">
+<section id={SectionName.Contributors} class="container mx-auto">
 	<div class="max-w-screen-sm mx-auto mb-8 text-center lg:mb-16">
 		<h1 class="pt-5 mb-4 text-4xl font-bold tracking-tight">Meet our Seki Devs</h1>
 		<p class="font-light lg:mb-16 sm:text-xl">
