9415974ec8bf6f5584aa0b85ee2b6542d41b9559
renaldox <akeripa.pose@gmail.com>
2024-02-28 21:56:45 +1300

    mobile responsive footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,35 +1,62 @@
 <script>
 	import { AppBar } from '@skeletonlabs/skeleton';
+	import { IconBrandDiscord, IconBrandFacebook, IconBrandGithub } from '@tabler/icons-svelte';
 </script>
 
-<AppBar>
+<AppBar shadow="shadow-2xl" slotTrail="!space-x-2 flex flex-wrap text-sm md:text-base lg:text-lg">
 	<svelte:fragment slot="lead">
-		<a
-			title="Dive into the code"
-			class="btn btn-sm variant-ghost-surface"
-			href="https://github.com/Samoa-Stack-Overflow"
-			target="_blank"
-		>
-			GitHub
-		</a>
-		&nbsp;&nbsp;
-		<a
-			title="Join us on Discord"
-			class="btn btn-sm variant-ghost-surface"
-			href="https://forms.gle/aoTeV9jF5h7wnqAG6"
-			target="_blank"
-		>
-			Discord
-		</a>
-		&nbsp;&nbsp;
-		<a
-			title="Get the latest on Facebook"
-			class="btn btn-sm variant-ghost-surface"
-			href="https://www.facebook.com/groups/948415479224570"
-			target="_blank"
-		>
-			Facebook
-		</a>
+		<div class="flex items-center space-x-4">
+			<a
+				title="Dive into the code"
+				class="hidden btn btn-sm variant-ghost-surface md:block"
+				href="https://github.com/Samoa-Stack-Overflow"
+				target="_blank"
+			>
+				GitHub
+			</a>
+
+			<a
+				target="_blank"
+				href="https://github.com/Samoa-Stack-Overflow"
+				class="lg:!ml-0 w-[32px] lg:w-auto overflow-hidden block md:hidden"
+			>
+				<IconBrandGithub />
+			</a>
+			&nbsp;&nbsp;
+			<a
+				title="Join us on Discord"
+				class="hidden btn btn-sm variant-ghost-surface md:block"
+				href="https://forms.gle/aoTeV9jF5h7wnqAG6"
+				target="_blank"
+			>
+				Discord
+			</a>
+
+			<a
+				target="_blank"
+				href="https://forms.gle/aoTeV9jF5h7wnqAG6"
+				class="lg:!ml-0 w-[32px] lg:w-auto overflow-hidden block md:hidden"
+			>
+				<IconBrandDiscord />
+			</a>
+			&nbsp;&nbsp;
+			<a
+				title="Get the latest on Facebook"
+				class="hidden btn btn-sm variant-ghost-surface md:block"
+				href="https://www.facebook.com/groups/948415479224570"
+				target="_blank"
+			>
+				Facebook
+			</a>
+
+			<a
+				target="_blank"
+				href="https://www.facebook.com/groups/948415479224570"
+				class="lg:!ml-0 w-[32px] lg:w-auto overflow-hidden block md:hidden"
+			>
+				<IconBrandFacebook />
+			</a>
+		</div>
 	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		Copyright © {new Date().getFullYear()} Samoa Stack Overflow. All Rights Reserved.
