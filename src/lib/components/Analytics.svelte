b5c8ba1e04a5b3f47ce8e75d369c5c6a9f71032b
Andre Westerlund <andrewest_04@hotmail.com>
2024-04-26 12:18:31 +1200

    update analytics

--- src/lib/components/Analytics.svelte
+++ src/lib/components/Analytics.svelte
@@ -1,14 +1,21 @@
 <script>
-	import { onMount } from 'svelte';
-
 	export let measurementId;
 
+	import { onMount } from 'svelte';
+
 	onMount(() => {
-		window.dataLayer = window.dataLayer || [];
-		function gtag() {
-			window.dataLayer.push(arguments);
-		}
-		gtag('js', new Date());
-		gtag('config', measurementId);
+		let script1 = document.createElement('script');
+		script1.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+		script1.async = true;
+		document.head.appendChild(script1);
+
+		let script2 = document.createElement('script');
+		script2.innerHTML = `
+					window.dataLayer = window.dataLayer || [];
+					function gtag(){dataLayer.push(arguments);}
+					gtag('js', new Date());
+					gtag('config', '${measurementId}');
+			`;
+		document.head.appendChild(script2);
 	});
 </script>
