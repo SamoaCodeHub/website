409ed248b5a857302076c20397b4269bd33cc107
Alesana Eteuati Jr <junior.eteuati@gmail.com>
2024-04-27 21:47:38 +1300

    [deps] upgrade packages

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import eslintPluginSvelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+
+export default [
+	// add more generic rule sets here, such as:
+	// js.configs.recommended,
+	...eslintPluginSvelte.configs['flat/recommended'],
+	{
+		rules: {
+			// override/add rules settings here, such as:
+			// 'svelte/rule-name': 'error'
+		},
+		languageOptions: {
+			ecmaVersion: 2022,
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				myCustomGlobal: 'readonly'
+			}
+		},
+		ignores: [
+			'.DS_Store',
+			'/node_modules',
+			'/build',
+			'/.svelte-kit',
+			'/package',
+			'.env',
+			'pnpm-lock.yaml',
+			'package-lock.json',
+			'yarn.lock',
+			'.vscode/settings.json',
+			'jsconfig.json',
+			'package.json'
+		]
+	}
+];
