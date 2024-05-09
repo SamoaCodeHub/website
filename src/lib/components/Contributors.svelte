57bea864578903c9beb81140c3ac3cd5e95bdf34
Andre Westerlund <andrewest_04@hotmail.com>
2024-05-09 23:18:28 +1200

    add founder status :technologist:

--- src/lib/components/Contributors.svelte
+++ src/lib/components/Contributors.svelte
@@ -17,7 +17,7 @@
 		{
 			avatar: Andre,
 			name: 'Andre Westerlund',
-			title: 'Senior Full Stack Developer',
+			title: 'Founder & Senior Full Stack Developer',
 			github: 'https://github.com/westerandr',
 			linkedin: 'https://www.linkedin.com/in/andrewesterlund/'
 		},
