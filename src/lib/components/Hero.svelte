c2505fd5188dc12e496d3fb5bd85f650e151a5fd
5thAttemptCode <henrycodes@outlook.com>
2024-09-23 15:45:03 +1300

    made requested changes for pr

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -27,7 +27,11 @@
 				<div
 					class="mt-10 w-[80%] lg:w-[100%] mx-auto xl:mx-0 lg:max-w-[600px] flex sm:flex-row flex-col items-center sm:gap-8 gap-4"
 				>
-					<div class="flex flex-1 justify-start pl-10 w-full sm:w-auto button-wrapper">
+<<<<<<< HEAD
+					
+=======
+					<div class="button-wrapper flex justify-start pl-10 flex-1 w-full sm:w-auto">
+>>>>>>> b7e16a2 (made requested changes for pr)
 						<p class="sr-only">
 							Press Join us Now to find out more about becoming a Samoa Code Hub Member
 						</p>
